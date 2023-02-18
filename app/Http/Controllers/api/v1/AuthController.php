3d35cb0bd8bd35719652fe82fa997a0d68cd0a9f
willam-gm <will-gm@hotmail.com>
2023-02-17 22:52:16 -0400

    Create authentification and register user

---
+++ app/Http/Controllers/api/v1/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $requestData  = $request->validate([
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:50',
+            'password' => 'required|confirmed'
+        ]);
+
+        $requestData['password'] = bcrypt($request->password);
+        $user = User::create($requestData);
+        $accessToken = $user->createToken('AuthToken')->accessToken;
+
+        return response([
+            'user' => $user,
+            'access_token' => $accessToken
+        ], 201);
+    }
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($credentials)) {
+            return response([
+                'message' => 'Email or password is invalid'
+            ], 401);
+        }
+
+        $user = auth()->user();
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response([
+            'user' => $user,
+            'access_token' => $accessToken
+        ], 200);
+    }
+
+    public function list(Request $request)
+    {
+        $users = User::all();
+        return response($users);
+    }
+}
